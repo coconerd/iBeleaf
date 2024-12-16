b3cb3b2e81133d2cac3db9adc4d843a7e503f7f6
hienphaang <thuyhienphanthi2004@gmail.com>
2024-12-16 23:37:52 +0700

    ✨ feat(database): include VoucherRuleSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +15,7 @@ class DatabaseSeeder extends Seeder
 			VoucherSeeder::class,
             OrderSeeder::class,
             ProductSeeder::class,
+            VoucherRuleSeeder::class
         ]);
     }
 }
