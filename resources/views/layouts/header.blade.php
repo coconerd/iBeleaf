ee710c2eb6f2eb03cc852b6408b6549cc9772647
ducmint864 <nddminh2021@gmail.com>
2024-12-20 15:50:17 +0700

    ✨ feat(header): update cart item count display logic based on user authentication

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -93,7 +93,11 @@
 							<i class="fa-solid fa-cart-shopping cart-icon" style="color: #1e362d;"></i>
 							<span class="cart-badge" id="cart-count"
 								data-cart-id="{{ Auth::user()->cart->cart_id ?? 0 }}">
-								{{ $totalQuantity ?? 0 }}
+								{{ 
+									Auth::check()
+									? Auth::user()->cart()->first()->items_count
+									: 0
+								}}
 							</span>
 						</a>
 					</div>
