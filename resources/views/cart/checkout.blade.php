9d80d802a0913f316ed5c1ac688f40c5b666407a
hienphaang <thuyhienphanthi2004@gmail.com>
2024-12-21 23:32:38 +0700

    ✨ feat(cart): enhance checkout UI

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -13,7 +13,6 @@
 @section('content')
 <div class="container mt-4 checkout-container">
     <div class="row">
-
         <!-- Shipping Address Section -->
         <div class="col-md-6 col-12 mb-4 mb-4 mb-md-0 left-body">
 			<div class="d-flex align-items-center">
@@ -59,29 +58,86 @@
 				<div class="mb-3">
 					<div class="form-check form-switch mb-3">
 						<input type="checkbox" class="form-check-input" id="defaultAddress" role="switch" aria-checked="false">
-						<label class="form-check-label" for="defaultAddress">Đặt làm địa chỉ mặc định</label>
+						<label class="form-check-label" for="defaultAddress">Đặt làm thông tin mặc định</label>
 					</div>
 					<button type="button" class="btn btn-custom mb-3">XÁC NHẬN ĐỊA CHỈ</button>
 				</div>
 			</form>
         </div>
 
-        <!-- Payment Method Section -->
-        <class class="col-md-6 col-12 right-body ps-md-5">
-			<!-- Shipping privacy-->
+        <div class="col-md-6 col-12 right-body ps-md-5">
+			<!--Order Summary-->
+			<div class="container mt-5" id="order-summary">
+				<div class="title-icon">
+					<span class="section-title">Thông tin đơn hàng</span>
+					<i class="fa-solid fa-seedling" style="color: #469636;"></i>
+				</div>
+				<hr class="line">
+				<div class="mt-4">
+					<div class="order-items">
+					<!-- Single Order Item -->
+						<div class="d-flex gap-3">
+							<div class="order-item-image position-relative">
+								<span class="item-quantity-badge">1</span>
+								<img src="https://via.placeholder.com/150" alt="Product Image">
+							</div>
+							<div class="order-item-details">
+								<h5 class="item-title">Tempting Tuna Cat Treats</h5>
+								<p class="item-variant">Brand: Temptations</p>
+								<div class="d-flex justify-content-between align-items-center">
+									<span class="item-price">$5.99</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="order-summary-details">
+						<div class="summary-row">
+							<span>Tạm tính (1 mặt hàng)</span>
+							<span>799.000 VNĐ</span>
+						</div>
+						<div class="summary-row">
+							<span>Phí vận chuyển</span>
+							<span id="shipping-fee">Chưa Tính Toán</span>
+						</div>
+						<div class="summary-row total">
+							<span>Tổng tiền (Đã bao gồm VAT)</span>
+							<span class="total-amount">799.000 VNĐ</span>
+						</div>
+					</div>
+
+					<button class="btn btn-custom w-100">Thanh toán</button>
+				</div>
+			</div>
+
+			<!--Shipping Privacy-->
 			<div id="shipping-privacy">
-            	<span class="section-title">Qui định về phí vận chuyển</span>
+				<div class="title-icon">
+					<span class="section-title">Qui định về phí vận chuyển</span>
+					<i class="fa-solid fa-seedling" style="color: #469636;"></i>
+				</div>
 				<hr class="line">
 				<div class="shipping-privacy-content">
-					<p>Phí vận chuyển sẽ được tính dựa trên địa chỉ giao hàng và loại sản phẩm bạn mua!</p>
-					<p><i class="fa-solid fa-circle-info" style="color: #c78b5e;"></i> Dieu 1</p>
-					<p><i class="fa-solid fa-circle-info" style="color: #c78b5e;"></i> Dieu 2</p>
-					<p><i class="fa-solid fa-circle-info" style="color: #c78b5e;"></i> Dieu 3</p>
+					<p>
+						<i class="fa-solid fa-circle-info" style="color: #c78b5e;"></i>
+						<span>Chỉ nhận giao ngoại thành đối với các sản phẩm là CHẬU</span>
+					</p>
+					<p>
+						<i class="fa-solid fa-circle-info" style="color: #c78b5e;"></i>
+						<span>Phí vận chuyển ngoại thành sẽ được tính dựa trên <b>cân nặng sản phẩm và địa điểm nhận hàng</b></span>
+					</p>
+					<p>
+						<i class="fa-solid fa-circle-info" style="color: #c78b5e;"></i>
+						<span>Phí vận chuyển nội thành TP.HCM đồng giá 30,000 VND</span>
+					</p>
 				</div>
 			</div>
 			
+			<!--Payment Method-->
 			<div id="payment-body">
-				<span class="section-title">Phương thức thanh toán</span>
+				<div class="title-icon">
+					<span class="section-title">Phương thức thanh toán</span>
+					<i class="fa-solid fa-seedling" style="color: #469636;"></i>
+				</div>
 				<hr class="line">
 				<p class="text-danger" id="address-warning">Vui lòng nhập địa chỉ giao hàng</p>
 					<div class="payment-method d-flex align-items-center justify-content-between disabled" class="type">
@@ -95,7 +151,6 @@
 						<input type="radio" name="payment" class="form-check-input">
 					</div>
 
-					<!-- Cash on Delivery -->
 					<div class="payment-method d-flex align-items-center justify-content-between disabled" class="type">
 						<div class="d-flex align-items-center">
 							<img src="https://www.muji.com.vn/_next/static/media/cod.6f018bcd.svg" alt="Cash">
@@ -104,51 +159,7 @@
 						<input type="radio" name="payment" class="form-check-input">
 					</div>
 			</div>
-			<!--Order Summary-->
-			<div class="container mt-5" id="order-summary">
-				<span class="section-title">Thông tin đơn hàng</span>
-				<hr class="line">
-				<div class="mt-4">
-					<div class="order-items">
-					<!-- Single Order Item -->
-					<div class="order-item">
-						<div class="d-flex gap-3">
-							<div class="order-item-image position-relative">
-								<span class="item-quantity-badge">1</span>
-								<img src="https://via.placeholder.com/150" alt="Product Image">
-							</div>
-							<div class="order-item-details flex-grow-1">
-								<div class="d-flex justify-content-between align-items-start">
-									<div>
-										<h5 class="item-title mb-1">Tempting Tuna Cat Treats</h5>
-										<p class="item-brand">Temptations</p>
-									</div>
-									<span class="item-price">$5.99</span>
-								</div>
-							</div>
-						</div>
-					</div>
-
-				<div class="order-summary-details">
-					<div class="summary-row">
-						<span>Tạm tính (1 mặt hàng)</span>
-						<span>799.000 VNĐ</span>
-					</div>
-					<div class="summary-row">
-						<span>Phí vận chuyển</span>
-						<span class="shipping-cost">Chưa Tính Toán</span>
-					</div>
-					<div class="summary-row total">
-						<span>Tổng tiền (Đã bao gồm VAT)</span>
-						<span class="total-amount">799.000 VNĐ</span>
-					</div>
-				</div>
-
-				<button class="btn btn-custom w-100">Thanh toán</button>
-				</div>
-			</div>
         </div>
     </div>
-</div>
 </div>
 @endsection
