3978737456a4ae7e2c72a34327a669e2a2d2b291
ducmint864 <nddminh2021@gmail.com>
2024-12-23 16:14:07 +0700

    ✨ feat(dashboard): add style and body-script sections to admin dashboard view

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,6 +1,12 @@
 @extends("admin.layouts.layout")
 @section("title", "Admin dashboard")
 
+@section('style')
+@endsection
+
 @section('content')
 <h1>This is admin dashboard</h1>
 @endsection
+
+@section('body-script')
+@endsections
