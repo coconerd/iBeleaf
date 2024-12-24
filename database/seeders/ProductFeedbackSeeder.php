2bc5c983a29130d610a02d21931982d33fc1b518
ducmint864 <nddminh2021@gmail.com>
2024-12-25 02:26:12 +0700

    ✨ feat(seeder): enhance ProductFeedbackSeeder with structured feedback generation for products

--- database/seeders/ProductFeedbackSeeder.php
+++ database/seeders/ProductFeedbackSeeder.php
@@ -10,58 +10,77 @@ use Faker\Factory as Faker;
 
 class ProductFeedbackSeeder extends Seeder
 {
-    public function run(): void
-    {
-        $faker = Faker::create('vi_VN');
-        
-        // Get all products and users
-        $products = Product::orderBy('product_id', 'asc')->limit(30)->get();;
-        $users = User::where('role_type', 0)->get();
-        
-        if ($users->isEmpty()) {
-            // Create some test users if none exist
-            for ($i = 0; $i < 5; $i++) {
-                $users->push(User::create([
-                    'full_name' => $faker->name,
-                    'email' => $faker->email,
-                    'user_name' => $faker->userName,
-                    'password' => bcrypt('password'),
-                    'role_type' => 0,
-                ]));
-            }
-        }
+	public function run(): void
+	{
+		$this->seedFeedbackFirstNProducts(30);
+		$this->seedFeedbackForBestSellingProducts(15);
+	}
 
-        foreach ($products as $product) {
-            // Generate 1-5 random feedback per product
-            $feedbackCount = rand(1, 5);
-            
-            for ($i = 0; $i < $feedbackCount; $i++) {
-                $stars = rand(3, 5); // Bias towards positive ratings
-                
-                $feedback = ProductFeedback::create([
-                    'product_id' => $product->product_id,
-                    'user_id' => $users->random()->user_id,
-                    'feedback_content' => $faker->realText(200),
-                    'num_star' => $stars,
-                ]);
+	protected function seedFeedbackFirstNProducts($productCount)
+	{
+		// Get all products and users
+		$products = Product::orderBy('product_id', 'asc')
+			->limit($productCount)
+			->get();
+		$this->seedFeedbackForProduct($products);
+	}
 
-                // 50% chance to add feedback images
-                if (rand(0, 1)) {
-                    $imageCount = rand(1, 3);
-                    for ($j = 0; $j < $imageCount; $j++) {
-                        $feedback->feedback_images()->create([
+	protected function seedFeedbackForBestSellingProducts($productCount)
+	{
+		// Get top N best selling products
+		$products = Product::orderBy('total_orders', 'desc')
+			->limit($productCount)
+			->get();
+		$this->seedFeedbackForProduct($products);
+	}
+
+	protected function seedFeedbackForProduct($products)
+	{
+		$faker = Faker::create('vi_VN');
+		$users = User::where('role_type', 0)->get();
+		if ($users->isEmpty()) {
+			// Create some test users if none exist
+			for ($i = 0; $i < 5; $i++) {
+				$users->push(User::create([
+					'full_name' => $faker->name,
+					'email' => $faker->email,
+					'user_name' => $faker->userName,
+					'password' => bcrypt('password'),
+					'role_type' => 0,
+				]));
+			}
+		}
+		foreach ($products as $product) {
+			// Generate 1-5 random feedback per product
+			$feedbackCount = rand(1, 5);
+
+			for ($i = 0; $i < $feedbackCount; $i++) {
+				$stars = rand(3, 5); // Bias towards positive ratings
+
+				$feedback = ProductFeedback::create([
+					'product_id' => $product->product_id,
+					'user_id' => $users->random()->user_id,
+					'feedback_content' => $faker->realText(200),
+					'num_star' => $stars,
+				]);
+
+				// 50% chance to add feedback images
+				if (rand(0, 1)) {
+					$imageCount = rand(1, 2);
+					for ($j = 0; $j < $imageCount; $j++) {
+						$feedback->feedback_images()->create([
 							'feedback_image' => file_get_contents(public_path('images/mock/feedback_' . rand(1, 5) . '.jpg'))
-                        ]);
-                    }
-                }
-            }
+						]);
+					}
+				}
+			}
 
-            // Update product rating statistics
-            $feedbacks = $product->product_feedbacks;
-            $product->update([
-                'total_ratings' => $feedbacks->count(),
-                'overall_stars' => $feedbacks->avg('num_star')
-            ]);
-        }
-    }
+			// Update product rating statistics
+			$feedbacks = $product->product_feedbacks;
+			$product->update([
+				'total_ratings' => $feedbacks->count(),
+				'overall_stars' => $feedbacks->avg('num_star')
+			]);
+		}
+	}
 }
